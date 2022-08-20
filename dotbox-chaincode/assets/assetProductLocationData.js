6db6d525588952563542605c7fb0804c821cbdf3
Andrews Tang <75391005+atang277@users.noreply.github.com>
2022-08-20 16:05:48 +0000

    Update assetProductLocationData.js

--- dotbox-chaincode/assets/assetProductLocationData.js
+++ dotbox-chaincode/assets/assetProductLocationData.js
@@ -2,7 +2,7 @@ const {v4:uuidv4} = require('uuid')
 
 class ProductLocationData{
     constructor(locationData){
-        this.locID=uuid();
+        this.locID=uuidv4();
         this.prevLocation=locationData.prevLocation;
         this.currLocation=locationData.currLocation;
         this.docType='locationData';
